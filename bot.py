5bd4fe1108a8ca30e30b2814affb818cceceee41
forlanjon9-star <forlanjon9@gmail.com>
2025-11-01 11:59:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")  # yoki to‘g‘ridan yoz: TOKEN = "123456:ABC..."
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Salom! Bot ishga tushdi ✅")
+
+bot.polling(non_stop=True)
 
